2e270092bc4bdc1c10778b391142422670b1280a
Luis <luis.huaranga.dev@gmail.com>
2024-09-06 18:34:03 -0500

    modificaciones 06-09-24

--- atajosTeclados.py
+++ atajosTeclados.py
@@ -1,18 +1,69 @@
 from pynput import keyboard
 import pyperclip  # Librería para manejar el portapapeles
 
+programa = "CDCB102"
+imagen = f"""
+_____________________$$$
+____________________$___$
+_____________________$$$
+_____________________$_$
+_____________________$_$
+___________________$$$_$$$
+_________________$$__$$$__$$$
+_______________$$__$$$$$$$___$
+______________$_______________$
+_____________$_________________$
+_____________$_________________$
+_____________$_____$$$$$$$$$$$$$$$
+_____________$____$_______________$
+_____________$____$___$$$$$$$$$$$$$
+_____________$___$___$___________$$$
+_____________$___$___$_$$$___$$$__$$        {programa}
+_____________$___$___$_$$$___$$$__$$
+_____________$___$___$___________$$$
+_____________$____$___$$$$$$$$$$$$$
+_____________$_____$$$$$$$$$$$$$$
+_____________$_________________$
+_____________$____$$$$$$$$$$$$$$
+_____________$___$__$__$__$__$
+_____________$__$$$$$$$$$$$$$$
+_____________$__$___$__$__$__$
+_____________$___$$$$$$$$$$$$$$$
+____________$$$_________________$$$
+__________$$___$$$_________$$$$$___$$
+________$$________$$$$$$$$$__________$$$
+_______$__$$_____________________$$$$___$$
+____$$$$$___$$$$$$$$______$$$$$$$_______$_$
+__$______$$_________$$$$$$______________$_$$
+_$____$____$____________________________$_$_$
+_$_____$___$______________$$$$$$$$$$$___$_$_$$
+_$$$____$___$__$$$$$$$$$$$$__________$___$_$_$$
+$___$$$$____$__$_____________________$___$_$$_$
+$$$____$___$$__$_____________________$$__$_$__$
+$___$__$__$$___$______________________$__$$$__$
+$_____$$_$$____$_______________$$$____$__$_$__$
+"""
+
+
 # Diccionario de palabras clave y sus reemplazos
 trigger_words = {
-    "dp": "a d d.lib.program(CDCB",  #programa
-    "ap": "a d a.lib.program(CDCB",
-    "ep": "a d e.lib.program(CDCB",
-    "dpr": "a d d.lib.proc(CDCP",    #proceso  
-    "epr": "a d a.lib.proc(CDCP",
-    "epr": "a d e.lib.proc(CDCP",
-    "dj": "a d d.lib.job(CDCA",      #job
-    "aj": "a d a.lib.job(CDCA",
-    "ej": "a d e.lib.job(CDCA",
-    "dc": "a d d.lib.copy.cob(",      #copy
+    "pr": programa,
+    "srt": "411997",                        #numero SRT
+    "luis": "S38457.LUIS.SRT11997",         #bibliotecaPersonal
+    "aco": f"C:\\Users\\HP\\Desktop\\Decomiso\\Grupo-4\\compilaciones\\{programa}.txt",
+    "luisp": f"S38457.LUIS.SRT11997({programa})",         #bibliotecaPersonal
+    "rexp": f"EXP DSN=S38457.LIB.PROGRAM({programa})",         #bibliotecaPersonal
+    "comp": f"SPRCC1.ISSYS.COMP.D.TIP012.{programa}.COBOL63",
+    "dp": "a d d.lib.program(",             #programa
+    "ap": "a d a.lib.program(",
+    "ep": "a d e.lib.program(",
+    "dpr": "a d d.lib.proc(",               #proceso   
+    "epr": "a d a.lib.proc(",
+    "epr": "a d e.lib.proc(",
+    "dj": "a d d.lib.job(A",                #job
+    "aj": "a d a.lib.job(",
+    "ej": "a d e.lib.job(",
+    "dc": "a d d.lib.copy.cob(",            #copy
     "ac": "a d a.lib.copy.cob(",
     "ec": "a d e.lib.copy.cob(",
     
@@ -51,11 +102,14 @@ def on_press(key):
 
 def on_release(key):
     # Salir del programa si se presiona Esc
-    if key == keyboard.Key.esc:
-        print("~~~~~~~~~~~~~~~~Fin del programa PIPIPPIPIPI~~~~~~~~~~~~~~~~")
+    if key == keyboard.Key.pause:
+        print(f"~~~~~~~~~~~~~~~~Fin del programa PROGRAMA ACTUAL: {programa}~~~~~~~~~~~~~~~~")
         return False
 
 # Iniciar el listener del teclado
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    print("-----------------Esperando palabras claves JIJIJIJIJI-----------------")
+    print("~~~~~~~~~~~~~~~~Esperando palabras PROGRAMA ACTUAL: {programa} claves JIJIJIJIJI~~~~~~~~~~~~~~~~")
+    print(imagen)
     listener.join()
+
+
